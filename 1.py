0e9c98d624207162757487ab5627d6ea2365cb01
LucianHitklif <asmetkina.va10@gmail.com>
2026-01-12 11:57:53 +0300

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,21 @@
+import random
+
+rows = random.randint(1, 20)
+cols = random.randint(1, 20)
+lists = [[random.randint(1, 100) for x in range(cols)] for y in range(rows)]
+
+print('Список: ')
+for row in lists:
+    print(row)
+
+max_val = lists[0][0]
+max_row, max_col = 0, 0
+
+for i in range(rows):
+    for j in range(cols):
+        if lists[i][j] > max_val:
+            max_val = lists[i][j]
+            max_row, max_col = i, j
+
+print(f'Максимальное число: {max_val}')
+print(f'Позиция: {max_row, max_col}')
